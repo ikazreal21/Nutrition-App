03fd64789091d4b72ee81c983440d497b5e1e8b8
ikazreal21 <expliot21@gmail.com>
2021-06-26 15:02:22 +0800

    Models Created

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Tasklist, name='home')
+    path('', views.Tasklist, name='home'),
+    path('food/', views.Nutrient, name='meal')
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+
+class Compute():
+    def __init__(self):
+        self.Total = []
+
+    def compute_nut(self, food, quantity):
+        for i in food:
+            self.Total.append(i*quantity)
+        return self.Total
 
 
 # Create your views here.
 def Tasklist(request):
     return HttpResponse("hello")
+
+
+def Nutrient(request):
+    nutri = Nutrients.objects.all()
+    nutriform = NutrientsForm()
+    if request.method == "POST":
+        nutrifom = NutrientsForm(request.POST)
+        if nutrifom.is_valid():
+            nutrifom.save()
+        return redirect("/")
+
+    context = {'form': nutriform, 'nutri': nutri}
+    return render(request, "task/food.html", context)
