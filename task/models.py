ebb228fcc82f1f1f859f3cd47b1e17e929bace16
ikazreal21 <expliot@gmail.com>
2021-06-27 12:57:19 +0800

    Models And Computation Fix

--- task/models.py
+++ task/models.py
@@ -2,20 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['complete']
-
-
-class Nutrients(models.Model):
+class Nutrient(models.Model):
     foodname = models.CharField(max_length=200)
     cal = models.FloatField(max_length=20)
     protien = models.FloatField(max_length=20)
--- task/views.py
+++ task/views.py
@@ -22,31 +22,31 @@ def is_number(s):
         return False
 
 
-# Create your views here.
-def Tasklist(request):
-    return HttpResponse("hello")
-
-
-
 def Index(request):
+    return render(request, "task/dashboard.html")
+
+
+def Calc(request):
     return render(request, "task/compute.html")
 
 
-def Nutrient(request):
+def Nutrients(request):
     quantity = request.POST['quantity']
- 
-    spinach = [23, 3, 0.3, 8100, 93]
+    spinach = Nutrient.objects.get(pk=1)
+    spinach = [spinach.cal, spinach.protien,
+               spinach.fat, spinach.vita, spinach.calcium]
 
     if quantity.isdigit():
         quanti = int(quantity)
         totalspin = Compute().compute_nut(spinach, quanti)
-        total = totalspin[0]
+        total = totalspin[2]
 
-        return render(request, "task/food.html", {"result": total})
+        return render(request, "task/compute.html", {"result": total})
+    elif not quantity:
+        return redirect('food')
     else:
         res = "Only digits are allowed"
-        return render(request, "task/food.html", {"result": res})
-
+        return render(request, "task/compute.html", {"result": res})
 
     # nutri = Nutrients.objects.all()
     # nutriform = NutrientsForm()
@@ -58,3 +58,7 @@ def Nutrient(request):
 
     # context = {'form': nutriform, 'nutri': nutri}
     # return render(request, "task/food.html", context)
+
+
+def Foodlist(request):
+    return render(request, "task/foodlist.html")
