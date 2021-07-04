1fea2c4c5e11c3065b4446b2bf1dba5672767561
ikazreal21 <expliot@gmail.com>
2021-07-04 23:31:09 +0800

    Random ID Change

--- task/models.py
+++ task/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import uuid
 
 # Create your models here.
 
@@ -12,6 +12,9 @@ class Nutrient(models.Model):
     fat = models.FloatField(max_length=20)
     vita = models.IntegerField()
     calcium = models.IntegerField()
+    rndid = models.CharField(max_length=100, default = uuid.uuid4,
+         editable = False, null=True, blank=True)
+
 
     def __str__(self):
         return self.foodname
